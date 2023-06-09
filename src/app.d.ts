43a34119a5eb57a819af06eebb7f9fa484b01602
falentio <riv.kevinfalent.io@gmail.com>
2023-06-09 19:10:19 +0700

    update app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,20 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import type { Application } from "$lib/server/application";
+import type { KVNamespace } from "@cloudflare/workers-types";
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			application: Application
+		}
 		// interface PageData {}
-		// interface Platform {}
+		interface Platform {
+			DATA: KVNamespace
+		}
 	}
 }
 
-export {};
+export { };
