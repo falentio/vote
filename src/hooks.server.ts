a84e2ccfdd9805e6d282a3c810063cf3433d0529
falentio <riv.kevinfalent.io@gmail.com>
2023-06-10 10:57:16 +0700

    fix build failling

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,3 +1,4 @@
+import { building } from "$app/environment"
 import { NODE_ENV } from "$env/static/private"
 import { Application } from "$lib/server/application"
 import type { KVNamespace as CFKVNamespace } from "@cloudflare/workers-types"
@@ -6,20 +7,21 @@ import type { Handle } from "@sveltejs/kit"
 let application: Application | null = null
 
 export const handle: Handle = async ({ event, resolve }) => {
-    if (!application) {
-        if (NODE_ENV === "production") {
-            if (!event.platform) {
-                throw new Error("platform not set")
-            }
-            application = new Application(event.platform)
-        } else {
-            const { FileStorage } = await import("@miniflare/storage-file")
-            const { KVNamespace } = await import("@miniflare/kv")
-            const storage = new FileStorage("./data")
-            application = new Application({
-                DATA: new KVNamespace(storage) as unknown as CFKVNamespace
-            })
+    if (building) {
+        return resolve(event)
+    }
+    if (NODE_ENV === "production") {
+        if (!event.platform) {
+            throw new Error("platform not set")
         }
+        application ||= new Application(event.platform)
+    } else {
+        const { FileStorage } = await import("@miniflare/storage-file")
+        const { KVNamespace } = await import("@miniflare/kv")
+        const storage = new FileStorage("./data")
+        application ||= new Application({
+            DATA: new KVNamespace(storage) as unknown as CFKVNamespace
+        })
     }
     event.locals.application = application
     return resolve(event)
